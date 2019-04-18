e7ded8d38a03cb1c9a37654f88a9d38c97907648
Ali Ariff <ali.ariff12@gmail.com>
2019-04-18 17:35:20 +0200

    implement pow

--- Exercise Sheet 1/ex4.hs
+++ Exercise Sheet 1/ex4.hs
@@ -1,5 +1,21 @@
 module S1Ex4 where
 
+-- | pow is helper function to implement ^
+--
+-- Examples:
+--
+-- >>> 0 `pow` 0
+-- 1
+--
+-- >>> 2 `pow` 3
+-- 8
+--
+-- >>> 4 `pow` 2
+-- 16
+pow :: Int -> Int -> Int
+pow _ 0 = 1
+pow x y = x * pow x (y - 1)
+
 -- | evaluates to xs to the power of ys interpreted as vectors, where the negative entries of ys are ignored
 --
 -- Examples:
@@ -22,5 +38,5 @@ module S1Ex4 where
 (^^^) [] [] = 1
 (^^^) [] _  = 0
 (^^^) _  [] = 0
-(^^^) (x : xs) (y : ys) | y > 0     = x ^ y * xs ^^^ ys
+(^^^) (x : xs) (y : ys) | y > 0     = x `pow` y * xs ^^^ ys
                         | otherwise = xs ^^^ ys
