0ba2a62a865fc837c5d56d6303d2bafc913e6ff9
Ali Ariff <ali.ariff12@gmail.com>
2019-05-02 21:11:31 +0200

    add degree func

--- Exercise Sheet 2/ex4.hs
+++ Exercise Sheet 2/ex4.hs
@@ -11,3 +11,16 @@ q = Coeff 4 3 (Coeff 2 1 (Coeff 5 0 Null))
 foldPoly :: (a -> Int -> b -> b) -> b -> Polynomial a -> b
 foldPoly _ e Null           = e
 foldPoly f e (Coeff x y xs) = f x y (foldPoly f e xs)
+
+-- |
+-- >>> degree q
+-- 3
+-- >>> degree (Coeff 5 0 Null)
+-- 0
+degree :: Polynomial Int -> Int
+degree Null = minBound :: Int
+degree xs   = foldPoly maxi 0 xs
+
+maxi :: a -> Int -> Int -> Int
+maxi _ x y | x > y     = x
+           | otherwise = y
