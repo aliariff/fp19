7b389295653a293afc10b4ae98bcf858e321d9ba
Ali Ariff <ali.ariff12@gmail.com>
2019-05-02 20:31:48 +0200

    solve ex3

---
+++ Exercise Sheet 2/ex3.hs
@@ -0,0 +1,23 @@
+module S2Ex3 where
+
+unique :: Eq a => a -> [a] -> [a]
+unique x y | filter (== x) y == [] = x : y
+           | otherwise             = y
+
+-- |
+-- >>> removeDuplicates [1,1,2,1,-1,1,1,2,3,1]
+-- [-1,2,3,1]
+removeDuplicates :: Eq a => [a] -> [a]
+removeDuplicates [] = []
+removeDuplicates xs = foldr unique [] xs
+
+len :: [a] -> Int
+len = foldr (\_ n -> 1 + n) 0
+
+-- |
+-- >>> differentDigits 08052019
+-- 6
+-- >>> differentDigits 111231112111
+-- 3
+differentDigits :: Int -> Int
+differentDigits x = len (removeDuplicates (show x))
